60594d197547b2633084a270200a8223b101094b
Gukin Han <gukin.dev@gmail.com>
2023-06-03 23:44:47 +0900

    Implement todo creating api

---
+++ src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user"; // temporary user id.
+
+            // (1) TodoEntity로 변환한다.
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            // (2) id를 null로 초기화한다. 생성 당시에는 id가 없어야 하기 때문이다.
+            entity.setId(null);
+
+            // (3) 임시 우저 아이디를 설정해준다. 이 부분은 4장 인증과 인가에서 수정할 예정이다. 지금은 인증과 인가 기능이 없으므로 한 유저(temporary-user)만 로그인 없이 사용 가능한 애플리케이션인 셈이다.
+            entity.setUserId(temporaryUserId);
+
+            // (4) 서비스를 이용해 Todo 엔티티를 생성한다.
+            List<TodoEntity> entities = service.create(entity);
+
+            // (5) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            // (6) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            // (7) ResponseDTO를 리턴한다.
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            // (8) 혹시 예외가 나는 경우 dto 대신 error에 메시지를 넣어 리턴한다.
+
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testService(); // 테스트 서비스 사용
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/test2")
+    public ResponseEntity<?> todoResponseEntity() {
+        List<TodoEntity> todoDTOList = new ArrayList<>();
+        TodoEntity todo = TodoEntity.builder()
+                .id("t-10328373")
+                .userId("developer")
+                .title("do what ever")
+                .build();
+        todoDTOList.add(todo);
+        todoDTOList.add(todo);
+
+        // adding logic
+        ResponseDTO<TodoEntity> response = ResponseDTO.<TodoEntity>builder().data(todoDTOList).build();
+        return ResponseEntity.ok().body(response);
+    }
+}
---
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -0,0 +1,52 @@
+package com.example.demo.service;
+
+import com.example.demo.TodoEntity;
+import com.example.demo.persistance.TodoRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class TodoService {
+
+    private final TodoRepository repository;
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        //validations
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity Id : {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    // 리팩토링한 메서드
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn("Unknown user");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+
+    public String testService() {
+        // TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        // TodoEntity 저장
+        repository.save(entity);
+        // TodoEntity 검색
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+        return savedEntity.getTitle();
+    }
+
+}

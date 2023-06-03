11f6f34cd9e0be05eae298c9f1f56dfddbe8b15e
Gukin Han <gukin.dev@gmail.com>
2023-06-04 01:32:36 +0900

    Add new feat: update todo entity

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -32,7 +33,25 @@ public class TodoService {
         return todoEntities;
     }
 
+    public List<TodoEntity> update(final TodoEntity entity) {
+        // (1) 저장할 엔티티가 유효한지 확인한다.
+        validate(entity);
 
+        // (2) 넘겨받은 엔티티 id를 이용해 TodoEntity를 가져온다.
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        if (original.isPresent()) {
+            // (3) 반환된 TodoEntity가 존재하면 값을 새 entity의 값으로 덮어 씌운다.
+            final TodoEntity todo = original.get();
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            // (4) 데이터베이스에 새 값을 저장한다.
+            repository.save(todo);
+        }
+        // Retrieve Todo에서 만든 메서드를 이용해 유저의 모든 Todo 리스트를 리턴한다.
+        return retrieve(entity.getUserId());
+    }
 
     // 리팩토링한 메서드
     private void validate(final TodoEntity entity) {
